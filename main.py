fcc73cf8cd99692811230db0a126ed127fd7c524
Buddalaz <sandunsampath289@gmail.com>
2022-05-13 22:24:48 +0530

    accessing query string request values

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ app_scope = "TEST APP scope"
 
 @app.route("/")
 def index():
+
+    # print(request)  # request is a class
+    print(request.args)  # accessing request arguments in url
+    print(request.args["test"])  # accessing request arguments by given the key in url http://127.0.0.1:5000/?test=1TEST%20TEST
+
     request_scope = "Test REQ scope"
     print(request_scope)
     print("Hello world 3")
