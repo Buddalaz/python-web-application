35e7901e36c80b4224b3a99a5862feac7cd8828e
Buddalaz <sandunsampath289@gmail.com>
2022-05-13 22:43:32 +0530

    convert string to integer of request argument

--- main.py
+++ main.py
@@ -8,14 +8,28 @@ app_scope = "TEST APP scope"
 @app.route("/")
 def index():
 
-    # print(request)  # request is a class
-    print(request.args)  # accessing request arguments in url
-    print(request.args["test"])  # accessing request arguments by given the key in url http://127.0.0.1:5000/?test=1TEST%20TEST
+    # request_scope = "Test REQ scope"
+    # print(request_scope)
 
-    request_scope = "Test REQ scope"
-    print(request_scope)
+    # print(request)  # request is a class
+    # print(request.args)  # accessing request arguments in url
+    # print(request.args["test"])  # accessing request arguments by given the key in url http://127.0.0.1:5000/?test=1TEST%20TEST
+
+    value = request.args["test"]
+    print(type(value))  # type is <class 'str'>
+
     print("Hello world 3")
     return "Hello world with debug"
+
+
+@app.route("/calc")  # http://127.0.0.1:5000/calc?number_1=12&number_2=5
+def calc():
+    number_one = request.args["number_1"]
+    number_two = request.args["number_2"]
+
+    result = int(number_one)+int(number_two) #addition of incoming request string values and convert to integer and return the sum 
+
+    return f"{result}" # we can't pass directly the int value
 
 
 print(app_scope)
