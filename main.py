4c4af9f58e1f7ded494a99511a65b905952e2fe2
Buddalaz <sandunsampath289@gmail.com>
2022-05-13 22:02:23 +0530

    accessing scope variable application wise and request wise

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+app_scope = "TEST APP scope"
+
+
 @app.route("/")
 def index():
-    return "Hello world"
+    request_scope = "Test REQ scope"
+    print(request_scope)
+    print("Hello world 3")
+    return "Hello world with debug"
+
+
+print(app_scope)
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(debug=True)
